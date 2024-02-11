26bf4b87bc95125b431bdc807158fae70132f344
Hunter <hunterbiu1205@gmail.com>
2024-02-11 19:12:56 +1030

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Archive from './pages/Archive/Archive';
 import NotFound from './pages/NotFound'; // Create a NotFound component
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <BrowserRouter>
+    <Routes>
         <Route path="/portfolio-V3" element={<Home />} />
         <Route path="/portfolio-V3/archive" element={<Archive />} />
-        {/* Catch-all route for any unknown paths */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+           {/* default redirect to home page */}
+           <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        </BrowserRouter>
   );
 }
 
