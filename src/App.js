18d82b42c6cf041bd9460af2679f3f02f183b253
Hunter <hunterbiu1205@gmail.com>
2024-02-11 18:57:15 +1030

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Archive from './pages/Archive/Archive';
-import ScrollToTop from './pages/ScrollToTop';
+import NotFound from './pages/NotFound'; // Create a NotFound component
 
 function App() {
   return (
     <Router>
-      <ScrollToTop />
       <Routes>
-        <Route path="/portfolio-V3" element={<Home />} />
-        <Route path="/portfolio-V3/archive" element={<Archive />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/archive" element={<Archive />} />
+        {/* Catch-all route for any unknown paths */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
