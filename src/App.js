4f15b98963b39c3085c2b5e3765d37f01e86d16f
Hunter <hunterbiu1205@gmail.com>
2024-02-11 18:50:24 +1030

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.scss';
-// Import HashRouter instead of BrowserRouter
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Archive from './pages/Archive/Archive';
 import ScrollToTop from './pages/ScrollToTop';
 
 function App() {
   return (
-    // Use HashRouter instead of BrowserRouter
     <Router>
       <ScrollToTop />
       <Routes>
