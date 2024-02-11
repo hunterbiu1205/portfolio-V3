6d6c277076e8ffea9049fc37b617c735f9f68cd0
Hunter <hunterbiu1205@gmail.com>
2024-02-11 19:19:02 +1030

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.scss';
 // Import HashRouter instead of BrowserRouter
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Archive from './pages/Archive/Archive';
-import ScrollToTop from './pages/ScrollToTop';
+import NotFound from './pages/NotFound'; // Adjust the path based on your project structure
 
 function App() {
+  // Use useEffect to check and redirect when the component mounts
+  useEffect(() => {
+    // Check if the current URL contains a hash
+    if (window.location.hash) {
+      // Extract the path without the hash
+      const pathWithoutHash = window.location.hash.substring(2);
+      
+      // Redirect to the path without the hash
+      window.location.href = pathWithoutHash;
+    }
+  }, []);
+
   return (
-    // Use HashRouter instead of BrowserRouter
     <Router>
-      <ScrollToTop />
       <Routes>
-        <Route path="/portfolio-V3" element={<Home />} />
-        <Route path="/portfolio-V3/archive" element={<Archive />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
